a3200326838a1bbb0bf43eae5ab009547b02f8db
Sharix111 <153104612+Sharix111@users.noreply.github.com>
2024-05-19 19:45:13 +0530

    Done

---
+++ app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
@@ -0,0 +1,41 @@
+package com.example.notessqlite
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notessqlite.databinding.ActivityAddNoteBinding
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabaseHelper
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title,content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved",Toast.LENGTH_SHORT).show()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notessqlite/UpdateActivity.kt
@@ -0,0 +1,54 @@
+package com.example.notessqlite
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notessqlite.databinding.ActivityUpdateBinding
+
+class UpdateActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var db: NotesDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1) {
+            finish()
+            return
+        }
+
+        try {
+            val note = db.getNoteByID(noteId)
+            binding.updateTitleEditText.setText(note.title)
+            binding.updateContentEditText.setText(note.content)
+        } catch (e: IllegalArgumentException) {
+            Toast.makeText(this, "Note not found", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+
+        binding.updateSaveButton.setOnClickListener {
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updatedNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updatedNote)
+            finish()
+            Toast.makeText(this, "Changes are Saved", Toast.LENGTH_SHORT).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
